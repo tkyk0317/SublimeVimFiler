04c8f0ba44440c9b3b6358f7028378b575749418
takayuki <takayuki.1981.0317@gmail.com>
2013-11-20 18:17:17 +0900

    [SublimeVimFiler] Add Limit Displayed Path Length.
    - Limit Length is 50 width.

--- VimFilerCommand.py
+++ VimFilerCommand.py
@@ -268,6 +268,8 @@ class FileSystemManager:
 
 class WriteResult:
 
+    LIMIT_PATH_LEN = 50
+
     @staticmethod
     def write(view, edit, dir_dict):
         # get width of view.
@@ -284,11 +286,11 @@ class WriteResult:
             per = dir_info_list[PERMISIION_INDEX]
             time = dir_info_list[UPDATE_TIME_INDEX]
 
-            sublime.status_message(owner)
             # write path.
-            WriteResult.__write_dir_name(view, edit, dir)
-            space_num = width - Utility.string_width(dir.decode(UTF8)) - \
-                len(owner) - len(per) - UPDATE_TIME_LEN - MARGIN
+            dir_len = WriteResult.__write_dir_name(view, edit, dir)
+
+            # insert space.
+            space_num = width - dir_len - len(owner) - len(per) - UPDATE_TIME_LEN - MARGIN
             WriteResult.insert_space(view, edit, space_num)
 
             # write owner and permission info.
@@ -307,7 +309,11 @@ class WriteResult:
 
     @staticmethod
     def __write_dir_name(view, edit, dir_name):
+        # check dir_name len.
+        if WriteResult.LIMIT_PATH_LEN < Utility.string_width(dir_name.decode(UTF8)):
+            dir_name = dir_name[:WriteResult.LIMIT_PATH_LEN]
         view.insert(edit, view.size(), dir_name)
+        return Utility.string_width(dir_name.decode(UTF8))
 
     @staticmethod
     def __write_owner(view, edit, owner):
